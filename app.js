cfd2ea649ec048e5988bc2934f9dd177359f2f8d
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-08-08 20:17:01 +0530

    feat: add displayBooks function
    This function turns book objects into cards and displays them in the page

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+let books = document.querySelector('#books')
+
 let myLibrary = [];
 
-function Book(title, author, page_number, reading_status) {
+function Book(title, author, pageNumber, readStatus) {
   this.title = title;
   this.author = author;
-  this.page_number = page_number;
-  this.reading_status = reading_status;
+  this.pageNumber = pageNumber;
+  this.readStatus = readStatus;
   this.info = function () {
-    return `${this.title} by ${this.author}, ${page_number} pages, ${reading_status}`;
+    return `${this.title} by ${this.author}, ${pageNumber} pages, ${readStatus}`;
   }
 }
 
@@ -15,9 +17,42 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+
+function displayBooks(myLibrary) {
+  myLibrary.forEach(book => {
+    let title = document.createElement('div');
+    title.textContent = book.title;
+    title.className = 'book-title';
+    
+    let author = document.createElement('div');
+    author.textContent = book.author;
+    author.className = 'book-author';
+    
+    let pageNumber = document.createElement('div');
+    pageNumber.textContent = book.pageNumber;
+    pageNumber.className = 'book-page-number';
+    
+    let readingStatus = document.createElement('div');
+    readingStatus.textContent = book.readStatus;
+    readingStatus.className = 'book-reading-status';
+    
+
+    let card = document.createElement('div');
+    card.className = 'card';
+
+    card.append(title, author, pageNumber, readingStatus);
+    books.appendChild(card);
+  });
+}
+
+
+
+
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'Not read yet');
 
 const book2 = new Book('Book 2','author2','20','read');
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(book2);
+
+displayBooks(myLibrary);
