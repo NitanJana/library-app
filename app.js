e683f6a0e6470cff9190c2b2008fd893b2a107e5
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-08-09 18:44:48 +0530

    refactor: move displayBooks code to addBookToLibrary
    code for adding book to library is in one func both from a storage purpose and from a visual purpose

--- app.js
+++ app.js
@@ -15,37 +15,28 @@ function Book(title, author, pageNumber, readStatus) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+
+  let title = document.createElement('div');
+  let author = document.createElement('div');
+  let pageNumber = document.createElement('div');
+  let readingStatus = document.createElement('div');
+
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pageNumber.textContent = book.pageNumber;
+  readingStatus.textContent = book.readStatus;
+  
+  title.className = 'book-title';
+  author.className = 'book-author';
+  pageNumber.className = 'book-page-number';
+  readingStatus.className = 'book-reading-status';
+
+  let card = document.createElement('div');
+  card.className = 'card';
+
+  card.append(title, author, pageNumber, readingStatus);
+  books.appendChild(card);
 }
-
-
-function displayBooks(myLibrary) {
-  myLibrary.forEach(book => {
-    let title = document.createElement('div');
-    title.textContent = book.title;
-    title.className = 'book-title';
-    
-    let author = document.createElement('div');
-    author.textContent = book.author;
-    author.className = 'book-author';
-    
-    let pageNumber = document.createElement('div');
-    pageNumber.textContent = book.pageNumber;
-    pageNumber.className = 'book-page-number';
-    
-    let readingStatus = document.createElement('div');
-    readingStatus.textContent = book.readStatus;
-    readingStatus.className = 'book-reading-status';
-    
-
-    let card = document.createElement('div');
-    card.className = 'card';
-
-    card.append(title, author, pageNumber, readingStatus);
-    books.appendChild(card);
-  });
-}
-
-
 
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'Not read yet');
@@ -55,4 +46,3 @@ const book2 = new Book('Book 2','author2','20','read');
 addBookToLibrary(theHobbit);
 addBookToLibrary(book2);
 
-displayBooks(myLibrary);
